014dbe11a3b2fc9a2bf5233997da10565305f295
leoyulitta <leoyulitta@gmail.com>
2023-06-01 20:22:44 -0300

    practico0 finalizado

---
+++ practico0/areaRectangulo.js
@@ -0,0 +1,8 @@
+let altura=3;
+let base=5;
+let area=base*altura;
+console.log("el area para una base de 5 y una altura de 3 es de:"+" "+area);
+let altura1=5.3;
+let base1=12.5;
+let area1=base1*altura1;
+console.log("el area para una base de 12.5 y una altura de 5.3 es de:"+" "+area1);
---
+++ practico0/promedio.js
@@ -0,0 +1,18 @@
+let n1=5;
+let n2=7;
+let n3=9;
+let p1=(n1+n2+n3)/3;
+
+let n4=-2;
+let n5=-3;
+let n6=-8;
+let p2=(n4+n5+n6)/3;
+
+let n7=-1.2;
+let n8=5.3;
+let n9=-3.3;
+let p3=(n7+n8+n9)/3;
+
+console.log("el promedio entre 5,7,9 es:"+p1);
+console.log("el promedio entre -2,-3,-8 es:"+p2);
+console.log("el promedio entre -1.2,5.3,-3.3 es:"+p3);
