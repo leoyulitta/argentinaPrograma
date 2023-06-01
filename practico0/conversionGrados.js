9844a451410c263b010c1fb2385a17ac4d78d574
leoyulitta <leoyulitta@gmail.com>
2023-06-01 20:20:10 -0300

    se agrega el archivo conversionGrados.js

---
+++ practico0/conversionGrados.js
@@ -0,0 +1,6 @@
+let f1=50;
+let c1=(f1-32)*5/9;
+console.log("50 grados Farenheit equivalen a:"+" "+c1+"°");
+let f2=75;
+let c2=(f2-32)*5/9;
+console.log("75 grados Farenheit equivalen a:"+" "+c2+"°");
