7fc7b9df9a83620b64bbfa421b0168358d56a049
leoyulitta <leoyulitta@gmail.com>
2023-06-15 21:11:23 -0300

    se modifica operaciones aritmeticas

--- practico1/operacionesAritmeticas.js
+++ practico1/operacionesAritmeticas.js
@@ -2,10 +2,15 @@ let a=11
 let b=11
 let c=4
 let d=2
-
 let suma=a+b
 let resta=c-d
 let resultadoFinal=suma*resta
-let esPar=resultadoFinal%2
-console.log("Mis variables iniciales fueron:"+" "+a+","+b+","+c+","+d+"."+" "+"La respuesta a verificar si el resultado final es par es:"+" "+esPar)
 
+function funcionEsPar(resultadoFinal){
+    return resultadoFinal%2===0
+}
+
+let esPar=funcionEsPar(resultadoFinal);
+
+console.log("Mis variables iniciales fueron:"+" "+a+","+b+","+c+","+d+".")
+console.log("La respuesta a verificar si el resultado final es par es:"+" "+esPar)
